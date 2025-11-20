4789403966ab912552de69e212dc9f48be1e018c
peng0o0 <tomato.202504.penghc@gmail.com>
2025-11-20 14:23:59 +0900

    user-model.js 更新

--- SERVER/models/user-model.js
+++ SERVER/models/user-model.js
@@ -1,7 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //載入 Mongoose 套件，之後你才能：
+// 連MongoDB/建立 Schema/建立 Model/做資料 CRUD
 const { Schema } = mongoose;
-const userSchema = new Schema{
-    username: {
-        
-    }
-}
+//從 mongoose 裡面取出 Schema 這個屬性，放進變數 Schema。
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor"],
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// enum用來限制 在 Schema 中用來「限制欄位的可選值」。
+// default 當欄位沒有提供值時，自動填入的預設值。
